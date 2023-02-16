a8bb16ef6bdad2688ce88c9edd7f81edf24013e8
Dikky <diki@hseo.co.id>
2023-02-17 00:53:43 +0700

    change user git

--- install/hanyassh.sh
+++ install/hanyassh.sh
@@ -3,7 +3,7 @@
 # 2022
 # ==================================================
 
-GitUser="syapik96"
+GitUser="FosterG4"
 #wget https://github.com/${GitUser}/
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
